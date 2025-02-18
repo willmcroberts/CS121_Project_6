abf2cee9ae164d9638efe12d4079facc4baeb8a0
willmcroberts <williammcroberts15@gmail.com>
2025-02-18 16:46:23 +0000

    Added makefile and test in main.cpp

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "date.h"
+#include "address.h"
+#include "student.h"
+
+void testAddress();
+void testDate();
+void testStudent();
+
+int main(){
+  std::cout << "Hello!" << std::endl;
+  testAddress();
+  testDate();
+  testStudent();
+  return 0;
+} // end main
+
+void testAddress(){
+  Address a;
+  a.init("123 W Main St", "Muncie", "IN", "47303");
+  a.printAddress();
+} // end testAddress
+
+void testDate(){
+ Date d;
+ d.init("01/27/1997");
+ d.printDate();
+} // end testDate
+
+void testStudent(){
+  std::string studentString = "Danielle,Johnson,32181 Johnson Course Apt. 389,New Jamesside,IN,59379,02/17/2004,05/15/2027,65";
+  Student* student = new Student();
+  student->init(studentString);
+  student->printStudent();
+  std::cout << std::endl;
+  std::cout << student->getLastFirst();
+  delete student;
+} // end testStudent
